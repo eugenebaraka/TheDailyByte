fa631d1c4637d44b1e8bdc172975063b321930f2
Eugene <eugenebaraka@gmail.com>
2023-04-10 10:08:18 -0400

    faster implementation

--- src/JewelsAndStones.py
+++ src/JewelsAndStones.py
@@ -11,6 +11,18 @@
 def jewels_and_stones(jewels:str, stones:str) -> int:
     return len([s for s in stones if s in jewels])
 
+# Time complexity O(n)
+# Space complexity O(n)
+
+def jewels_and_stones(jewels: str, stones: str) -> int:
+    # create unique jewels, membership testing in a set is faster than searching a string (O(1))
+    jewel_set = set(jewels) 
+    return sum(1 for stone in stones if stone in jewel_set)
+
+# Time complexity: also O(n) -- still have to examine each character in stones
+# Space complexity O(n) # set created
+
+
 # Tests
 print(jewels_and_stones(jewels="abc", stones="ac"))
 print(jewels_and_stones(jewels="Af", stones="AaaddfFf"))
